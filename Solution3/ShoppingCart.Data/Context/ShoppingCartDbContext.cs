5e47d3ba423808d5e0db12601b641f097eac27a4
KIRSTIAN XUEREB <kirstian.xuereb.e23794@mcast.edu.mt>
2021-01-21 19:53:41 +0100

    First Migration :')

---
+++ Solution3/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingCart.Domain.Models;
+
+namespace ShoppingCart.Data.Context
+{
+    public class ShoppingCartDbContext : DbContext
+    {
+        public ShoppingCartDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
